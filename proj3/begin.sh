bbffce5f76cffab32db65bcf27b45279ef9bbda8
LLipter <katsudonn@163.com>
2019-04-13 14:13:10 -0700

    proj3 ready to go\!

--- proj3/begin.sh
+++ proj3/begin.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-virtualenv -p python3 venv || python3 -m venv venv || python -m venv venv
-source venv/bin/activate || source venv/Scripts/activate
-pip install --requirement requirements.txt
+pip3 install --requirement requirements.txt
 FLASK_APP=server.py flask run
